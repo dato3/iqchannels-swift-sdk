2dff155a50430f1a36d480f4e176dcf8c5cf05a2
Daulet Tokmukhanbet <61043918+dato3@users.noreply.github.com>
2024-03-13 11:46:38 +0500

    pod init

---
+++ Example/IQChannelsSwift/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  IQChannelsSwift
+//
+//  Created by Daulet Tokmukhanbet on 03/13/2024.
+//  Copyright (c) 2024 Daulet Tokmukhanbet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
