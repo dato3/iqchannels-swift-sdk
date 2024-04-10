d4efbc527ab908bb4d584734b20607d2401a33a3
Aralbek Muhammed <aralbekmuhammed.2007@gmail.com>
2024-04-10 19:57:57 +0500

    Assets relocated

---
+++ IQChannelsSwift/Classes/IQScrollDownButton.swift
@@ -0,0 +1,20 @@
+//
+//  IQScrollDownButton.swift
+//  IQChannelsSwift
+//
+//  Created by Muhammed Aralbek on 10.04.2024.
+//
+
+import UIKit
+
+class IQScrollDownButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
